0dfed68ea38da17807e02960f904e5c75107ec38
Chris Wang <mw866@cornell.edu>
2017-01-18 19:08:32 +0000

    deleted ./mysite and consilidate into ./squawker

--- squawker/settings.py
+++ squawker/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'mysite.urls'
+ROOT_URLCONF = 'squawker.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'mysite.wsgi.application'
+WSGI_APPLICATION = 'squawker.wsgi.application'
 
 
 # Database
