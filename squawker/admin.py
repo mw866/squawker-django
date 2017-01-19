66d890f849365b881117f204961eceaa3ed96910
Chris Wang <mw866@cornell.edu>
2017-01-19 20:49:42 +0000

    import admin package

--- squawker/admin.py
+++ squawker/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib import admin
 from .models import Post
 
-
+# Reference: https://docs.djangoproject.com/en/1.10/intro/tutorial02/#make-the-poll-app-modifiable-in-the-admin
 class PostAdmin(admin.ModelAdmin):
     list_display = ['post_text', 'post_date']
 
--- squawker/urls.py
+++ squawker/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
+from django.contrib import admin
 from . import views
+
 
 app_name = 'squawker'
 urlpatterns = [
